3dcc4a2a8957016d45eedd74ca7527f7df35ef3a
Mochamad Rizky Cahya Diputra <moch.rizky.cahya@gmail.com>
2022-05-07 19:14:45 +0700

    Add navbar component

---
+++ src/scripts/Components/Navbar/Navbar.js
@@ -0,0 +1,32 @@
+import './Navbar.scss';
+import logo from '../../../public/images/logo/restaurant.png';
+
+class Navbar extends HTMLElement {
+  connectedCallback() {
+    this.render();
+  }
+
+  render() {
+    this.innerHTML = `
+      <nav class="nav">
+        <img src="${logo}" alt="Hunger Apps logo" class="nav__logo">
+
+        <a href="#" id="hamburger" class="nav-hamburger__button">☰</a>
+
+        <ul class="nav__list">
+          <li class="nav__item">
+            <a href="/" class="nav__link">Home</a>
+          </li>
+          <li class="nav__item">
+            <a href="#" class="nav__link">Favorite</a>
+          </li>
+          <li class="nav__item">
+            <a href="#" class="nav__link" target="_blank">About US</a>
+          </li>
+        </ul>
+      </nav>
+    `;
+  }
+}
+
+customElements.define('navbar-component', Navbar);
