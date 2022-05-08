842b43846e2a731587b09de7e82b5a0cef459be2
Mochamad Rizky Cahya Diputra <moch.rizky.cahya@gmail.com>
2022-05-08 18:10:19 +0700

    Add list restaurant component and card item into the list container

---
+++ src/scripts/Components/ListRestaurant/ListRestaurant.js
@@ -0,0 +1,32 @@
+import './ListRestaurant.scss';
+import './RestaurantCardItem/RestaurantCardItem';
+
+import Data from '../../../DATA.json';
+
+class ListRestaurant extends HTMLElement {
+  connectedCallback() {
+    this.render();
+
+    this.listElementContainer = this.querySelector('.list-restaurant__container');
+
+    Data.restaurants.forEach((item) => {
+      const restaurantCard = document.createElement('restaurant-card-item');
+      restaurantCard.className = 'restaurant-card-item';
+      restaurantCard.restaurant = item;
+      
+      this.listElementContainer.appendChild(restaurantCard);
+    });
+  }
+
+  render() {
+    this.innerHTML = `
+      <section class="list-restaurant">
+        <h2>List Restaurant</h2>
+        <div class="list-restaurant__container">
+        </div>
+      </section>
+    `;
+  }
+}
+
+customElements.define('list-restaurant-component', ListRestaurant);
