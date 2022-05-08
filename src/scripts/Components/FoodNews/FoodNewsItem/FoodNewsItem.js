81e59616b560a486dca365c9c0a781f49bbbe3c6
Mochamad Rizky Cahya Diputra <moch.rizky.cahya@gmail.com>
2022-05-08 23:04:47 +0700

    Add food news item card

---
+++ src/scripts/Components/FoodNews/FoodNewsItem/FoodNewsItem.js
@@ -0,0 +1,23 @@
+import './FoodNewsItem.scss';
+
+class FoodNewsItem extends HTMLElement {
+  set foodNews(value) {
+    this._config = value;
+    this.render();
+  }
+
+  render() {
+    const { name: title, url: linkArticle, image, description } = this._config;
+    this.innerHTML = `
+      <img src="${image?.thumbnail?.contentUrl}" width="100%" alt="${title}">
+      <div class="card-item__description">
+        <a href="${linkArticle}" class="card-item__description-link" aria-label="article ${title} ">${title}</a>
+        <p>
+          ${description}
+        </p>
+      </div>
+    `;
+  }
+}
+
+customElements.define('food-news-card-item', FoodNewsItem);
