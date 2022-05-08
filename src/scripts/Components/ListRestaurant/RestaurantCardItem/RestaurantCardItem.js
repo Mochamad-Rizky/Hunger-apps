426c72fb0da43aa55d35206aeab62d24851095f9
Mochamad Rizky Cahya Diputra <moch.rizky.cahya@gmail.com>
2022-05-08 23:47:29 +0700

    Update alt attribute

--- src/scripts/Components/ListRestaurant/RestaurantCardItem/RestaurantCardItem.js
+++ src/scripts/Components/ListRestaurant/RestaurantCardItem/RestaurantCardItem.js
@@ -10,7 +10,7 @@ class RestaurantCardItem extends HTMLElement {
     const { id, name, description, pictureId, city, rating } = this._config;
 
     this.innerHTML = `
-      <img src="${pictureId}" width="100%" alt="${name} ${city}">
+      <img src="${pictureId}" width="100%" alt="${name} ${city} image">
       <div class="card-item__description">
         <p class="card-item__description-rating">Rating : ${rating}</p>
         <a href="/${id}" aria-label="article info restaurant ${name} ${city}">${name} - ${city}</a>
