07183a5e28cc4726b4ecf4320398b989b34c5af7
Mochamad Rizky Cahya Diputra <moch.rizky.cahya@gmail.com>
2022-05-08 23:04:22 +0700

    Add list food news component

---
+++ src/scripts/Components/FoodNews/FoodNews.js
@@ -0,0 +1,35 @@
+import './FoodNewsItem/FoodNewsItem';
+import './FoodNews.scss';
+import FoodNewsResource from '../../data/food-news-resource';'../../data/food-news-resource';
+
+class FoodNews extends HTMLElement {
+  async connectedCallback() {
+    try {
+      this.getData = await FoodNewsResource.getFoodNews('food', 3);
+      this.render();
+      this.listElementContainer = this.querySelector('.list-food-news__container');
+    } catch (error) {
+      this.listElementContainer.innerHTML = "<h2 style='color: white;'>U have an error</h2>"
+    }
+    
+    this.getData.value.forEach((item) => {
+      const foodNewsCard = document.createElement('food-news-card-item');
+      foodNewsCard.className = 'card-item';
+      foodNewsCard.foodNews = item;
+      
+      this.listElementContainer.appendChild(foodNewsCard);
+    });
+  }
+
+  render() {
+    this.innerHTML = `
+      <section>
+        <h2>Food News</h2>
+        <div class="list-food-news__container">
+        </div>
+      </section>
+    `;
+  }
+}
+
+customElements.define('food-news-component', FoodNews);
--- src/scripts/index.js
+++ src/scripts/index.js
@@ -6,6 +6,7 @@ import './Components/Navbar/Navbar';
 import './Components/Hero/Hero';
 import './Components/ListRestaurant/ListRestaurant';
 import './Components/Promotion/Promotion';
+import './Components/FoodNews/FoodNews';
 
 const hamburgerButton = document.getElementById('hamburger');
 
