62d34c266808947b4a370eb4fe15a73c69b08af4
Mochamad Rizky Cahya Diputra <moch.rizky.cahya@gmail.com>
2022-05-09 00:17:20 +0700

    Remove copy webpack plugin and add custom output file images

--- webpack.common.js
+++ webpack.common.js
@@ -1,12 +1,12 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const CopyPlugin = require('copy-webpack-plugin');
 
 module.exports = {
   entry: path.resolve(__dirname, 'src/scripts/index.js'),
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: '[name].bundle.js',
+    assetModuleFilename: 'images/[name][ext][query]',
   },
   module: {
     rules: [
@@ -29,13 +29,5 @@ module.exports = {
       template: path.resolve(__dirname, 'src/templates/index.html'),
       filename: "index.html",
     }),
-    new CopyPlugin({
-      patterns: [
-        {
-          from: path.resolve(__dirname, 'src/public/'),
-          to: path.resolve(__dirname, 'dist/'),
-        },
-      ],
-    })
   ],
 };
