3481efcd9ea9825d5c2e56541c8fda8f079fb6d5
Mochamad Rizky Cahya Diputra <moch.rizky.cahya@gmail.com>
2022-05-18 17:37:19 +0700

    Remove looping for add card item

--- src/scripts/views/Components/ListRestaurant/ListRestaurant.js
+++ src/scripts/views/Components/ListRestaurant/ListRestaurant.js
@@ -1,31 +1,16 @@
 import './ListRestaurant.scss';
 import './Responsive.scss';
-import './RestaurantCardItem/RestaurantCardItem';
-
-import Data from '../../../../DATA.json';
 
 class ListRestaurant extends HTMLElement {
   connectedCallback() {
     this.render();
-
-    this.listElementContainer = this.querySelector('.list-restaurant__container');
-
-    Data.restaurants.forEach((item) => {
-      const restaurantCard = document.createElement('restaurant-card-item');
-      restaurantCard.className = 'card-item';
-      restaurantCard.restaurant = item;
-      
-      this.listElementContainer.appendChild(restaurantCard);
-    });
   }
 
   render() {
     this.innerHTML = `
-      <section>
-        <h2>List Restaurant</h2>
-        <div class="list-restaurant__container">
-        </div>
-      </section>
+      <h2>List Restaurant</h2>
+      <div class="list-restaurant__container">
+      </div>
     `;
   }
 }
