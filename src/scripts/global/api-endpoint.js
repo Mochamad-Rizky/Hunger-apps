38aad2ef97c1b830088056434f101d65acd412d6
Mochamad Rizky Cahya Diputra <moch.rizky.cahya@gmail.com>
2022-05-18 17:33:15 +0700

    Add image base fetch url request

--- src/scripts/global/api-endpoint.js
+++ src/scripts/global/api-endpoint.js
@@ -3,6 +3,7 @@ import CONFIG from './config';
 const API_ENDPOINT = {
   FOOD_NEWS: (newsCategory, count) => `${CONFIG.BASE_URL_BING}/news/search?q=${newsCategory}&safeSearch=Off&textFormat=Raw&freshness=Day&count=${count}`,
   LIST: `${CONFIG.BASE_URL_RESTAURANT}list`,
+  IMAGE: `${CONFIG.BASE_URL_RESTAURANT}images/small/`,
   DETAIL: (id) => `${CONFIG.BASE_URL_RESTAURANT}detail/${id}`,
   POST_REVIEW: `${CONFIG.BASE_URL_RESTAURANT}review`,
 };
