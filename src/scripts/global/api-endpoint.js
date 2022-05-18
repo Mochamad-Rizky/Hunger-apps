9b506505998776e1ccec67fa504bcad130c2c6d9
Mochamad Rizky Cahya Diputra <moch.rizky.cahya@gmail.com>
2022-05-18 12:57:03 +0700

    Add configuration api

--- src/scripts/global/api-endpoint.js
+++ src/scripts/global/api-endpoint.js
@@ -1,7 +1,10 @@
 import CONFIG from './config';
 
 const API_ENDPOINT = {
-  FOOD_NEWS: (newsCategory, count) =>`${CONFIG.BASE_URL_BING}/news/search?q=${newsCategory}&safeSearch=Off&textFormat=Raw&freshness=Day&count=${count}`,
+  FOOD_NEWS: (newsCategory, count) => `${CONFIG.BASE_URL_BING}/news/search?q=${newsCategory}&safeSearch=Off&textFormat=Raw&freshness=Day&count=${count}`,
+  LIST: `${CONFIG.BASE_URL_RESTAURANT}list`,
+  DETAIL: (id) => `${CONFIG.BASE_URL_RESTAURANT}detail/${id}`,
+  POST_REVIEW: `${CONFIG.BASE_URL_RESTAURANT}review`,
 };
 
 export default API_ENDPOINT;
--- src/scripts/global/config.js
+++ src/scripts/global/config.js
@@ -5,6 +5,10 @@ const CONFIG = {
     'X-RapidAPI-Key': '63fe8c3f16mshf2bdaf632a20aa4p1c2018jsn0eacf3c88f48',
   },
   BASE_URL_BING: 'https://bing-news-search1.p.rapidapi.com',
-}
+  BASE_URL_RESTAURANT: 'https://restaurant-api.dicoding.dev/',
+  DATABASE_NAME: 'restaurant-db',
+  DATABASE_VERSION: 1,
+  OBJECT_STORE_NAME: 'restaurant',
+};
 
 export default CONFIG;
