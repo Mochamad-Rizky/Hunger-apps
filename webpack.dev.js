932e7b7a7881025f9aeef12fa2dfac9e44a7e3c9
Mochamad Rizky Cahya Diputra <moch.rizky.cahya@gmail.com>
2022-05-20 02:05:00 +0700

    Add client overlay configuration

--- webpack.dev.js
+++ webpack.dev.js
@@ -4,6 +4,12 @@ const common = require('./webpack.common');
 module.exports = merge(common, {
   mode: 'development',
   devServer: {
+    client: {
+      overlay: {
+        errors: true,
+        warnings: false,
+      },
+    },
     static: '.',
     historyApiFallback: true,
   },
