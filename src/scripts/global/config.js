d211cf667f010fbcfe22e7a98dd24ad44849a7fc
Mochamad Rizky Cahya Diputra <moch.rizky.cahya@gmail.com>
2022-05-20 02:46:07 +0700

    Update bing api key

--- src/scripts/global/config.js
+++ src/scripts/global/config.js
@@ -2,7 +2,7 @@ const CONFIG = {
   HEADERS_BING: {
     'x-bingapis-sdk': 'true',
     'X-RapidAPI-Host': 'bing-news-search1.p.rapidapi.com',
-    'X-RapidAPI-Key': '63fe8c3f16mshf2bdaf632a20aa4p1c2018jsn0eacf3c88f48',
+    'X-RapidAPI-Key': '7b7bf7765cmshc73e4af57fc59e1p101ae3jsn843302be0ca8',
   },
   BASE_URL_BING: 'https://bing-news-search1.p.rapidapi.com',
   BASE_URL_RESTAURANT: 'https://restaurant-api.dicoding.dev/',
