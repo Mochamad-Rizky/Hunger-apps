ef7d6601f96c7e1e4ea45cf1402d2fb802318c06
Mochamad Rizky Cahya Diputra <moch.rizky.cahya@gmail.com>
2022-05-19 14:53:40 +0700

    Add item favorite restaurant from idb into favorite page

--- src/scripts/views/pages/Favorite.js
+++ src/scripts/views/pages/Favorite.js
@@ -1,17 +1,34 @@
+import FavRestaurantIdb from '../../data/favorite-restaurant-idb';
+import error from '../UI/Error/error';
+import '../Components/ListRestaurant/ListRestaurant';
+import '../Components/ListRestaurant/RestaurantCardItem/RestaurantCardItem';
+import '../Components/Promotion/Promotion';
+
 const Favorite = {
   async render() {
     return `
-      <div class="favorite">
-        <h1>Favorite</h1>
-        <p>
-          This is the Favorite page.
-        </p>
-      </div>
+      <list-restaurant-component class="list-restaurant" title="Favorite Restaurant">
+      
+      </list-restaurant-component>
     `;
   },
   
   async afterRender() {
-    console.log('favorite page');
+    const data = await FavRestaurantIdb.getAllRestaurants();
+    const restaurantContainer = document.querySelector('.list-restaurant__container');
+    
+    if (data.length === 0) {
+      restaurantContainer.innerHTML = error('U don\'t have any favorite restaurant');
+      restaurantContainer.style.display = 'block';
+      return;
+    }
+    
+    data?.forEach((restaurant) => {
+      const restaurantCardItem = document.createElement('restaurant-card-item');
+      restaurantCardItem.className = 'card-item';
+      restaurantCardItem.restaurant = restaurant;
+      restaurantContainer.appendChild(restaurantCardItem);
+    });
   },
 };
 
