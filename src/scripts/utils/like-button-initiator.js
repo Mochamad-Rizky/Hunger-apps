c452569bf2cb89cdf241cf5208eb485dba9f0a1d
Mochamad Rizky Cahya Diputra <moch.rizky.cahya@gmail.com>
2022-05-19 14:16:17 +0700

    Add like buton initiator for checking idb database

---
+++ src/scripts/utils/like-button-initiator.js
@@ -0,0 +1,48 @@
+import { createLikeButtonTemplate, createLikedButtonTemplate } from '../views/UI/Buttons/button-favorite-template';
+
+const LikeButtonInitiator = {
+  async init({ likeButtonContainer, favoriteRestaurant, data }) {
+    this._likeButtonContainer = likeButtonContainer;
+    this._favoriteRestaurant = favoriteRestaurant;
+    this._restaurant = data.restaurant;
+    
+    await this._renderButton();
+  },
+  
+  async _renderButton() {
+    const { id } = this._restaurant;
+    
+    if (await this._isRestaurantExist(id)) {
+      this._renderLiked();
+    } else {
+      this._renderLike();
+    }
+  },
+  
+  async _isRestaurantExist(id) {
+    const restaurant = await this._favoriteRestaurant.getRestaurant(id);
+    return !!restaurant;
+  },
+  
+  _renderLike() {
+    this._likeButtonContainer.innerHTML = createLikeButtonTemplate();
+    
+    const likeButton = document.querySelector('#likeButton');
+    likeButton.addEventListener('click', async () => {
+      await this._favoriteRestaurant.putRestaurant(this._restaurant);
+      await this._renderButton();
+    });
+  },
+  
+  _renderLiked() {
+    this._likeButtonContainer.innerHTML = createLikedButtonTemplate();
+    
+    const likeButton = document.querySelector('#likeButton');
+    likeButton.addEventListener('click', async () => {
+      await this._favoriteRestaurant.deleteRestaurant(this._restaurant.id);
+      await this._renderButton();
+    });
+  },
+};
+
+export default LikeButtonInitiator;
