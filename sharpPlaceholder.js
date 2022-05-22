b124aaf76c9e89701a2be331df6605514e60b364
Mochamad Rizky Cahya Diputra <moch.rizky.cahya@gmail.com>
2022-05-23 01:12:12 +0700

    Add sharp configuration for build responsive images

---
+++ sharpPlaceholder.js
@@ -0,0 +1,26 @@
+const sharpHero = require('sharp');
+const fs = require('fs');
+const path = require('path');
+
+const target = path.resolve(__dirname, 'src/public/images/Universal');
+const destination = path.resolve(__dirname, 'src/public/images/Universal');
+
+if (!fs.existsSync(destination)) {
+  fs.mkdirSync(destination);
+}
+
+fs.readdirSync(target)
+  .forEach((image) => {
+    
+    sharpHero(`${target}/${image}`)
+      .resize(800)
+      .toFile(path.resolve(__dirname, `${destination}/${image.split('.')
+        .slice(0, -1)
+        .join('.')}-large.jpg`));
+    
+    sharpHero(`${target}/${image}`)
+      .resize(480)
+      .toFile(path.resolve(__dirname, `${destination}/${image.split('.')
+        .slice(0, -1)
+        .join('.')}-small.jpg`));
+  });
