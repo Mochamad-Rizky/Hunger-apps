1bbd63e83c1164937bb6af5ba8252b1398281c76
Mochamad Rizky Cahya Diputra <moch.rizky.cahya@gmail.com>
2022-05-23 01:11:39 +0700

    Add status if register successfully

--- src/scripts/utils/sw-register.js
+++ src/scripts/utils/sw-register.js
@@ -1,6 +1,7 @@
 const swRegister = async () => {
   if ('serviceWorker' in navigator) {
     await navigator.serviceWorker.register('/sw.js');
+    console.log('service worker registered');
   } else {
     console.log('Service worker not supported');
   }
